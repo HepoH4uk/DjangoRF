215f65f9f1955d4b5924e41ad4b9a027f83a6ae0
AlTim <aleks00_777@mail.ru>
2025-08-14 19:43:52 +0300

    update materials

--- materials/views.py
+++ materials/views.py
@@ -1,4 +1,6 @@
 from rest_framework.viewsets import ModelViewSet, generics
+
+from users.permissions import IsModer, IsOwner
 from .models import Course, Lesson
 from .serializers import CourseSerializer, LessonSerializer
 
@@ -8,6 +10,30 @@ class CourseViewSet(ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    def get_permissions(self):
+        if self.action in ["create", "destroy"]:
+            self.permission_classes = (~IsModer,)
+        elif self.action in ["update", "retrieve"]:
+            self.permission_classes = (~IsModer | IsOwner,)
+        elif self.action == "destroy":
+            self.permission_classes = (~IsModer, IsOwner,)
+        return super().get_permissions()
+
+class LessonViewSet(ModelViewSet):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    def get_permissions(self):
+        if self.action in ["create", "destroy"]:
+            self.permission_classes = (~IsModer,)
+        elif self.action in ["update", "retrieve"]:
+            return super().get_permissions()
 
 class LessonListCreateAPIView(generics.ListCreateAPIView):
     queryset = Lesson.objects.all()
